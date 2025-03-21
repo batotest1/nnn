6811676fb54a5f5b7b615710d9298d7c8bd03727
batotest1 <muxammadjon10554534@gmail.com>
2025-03-21 11:16:14 +0500

    yangilandi

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,72 +1,211 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(MaterialApp(
+    home: MyHomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  // Create controllers for each TextField
+  final _nameController = TextEditingController();
+  final _surnameController = TextEditingController();
+  final _dayController = TextEditingController();
+  final _monthController = TextEditingController();
+  final _yearController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
+      appBar: AppBar(title: Text('User Info')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: TextField(
+                controller: _nameController, // Assign controller
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: "Ism kiriting",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: TextField(
+                controller: _surnameController, // Assign controller
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: "Familiya kiriting",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: TextField(
+                    controller: _dayController, // Assign controller
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "kun",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: TextField(
+                    controller: _monthController, // Assign controller
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "oy",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: TextField(
+                    controller: _yearController, // Assign controller
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: "yil",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Pass the entered data to the next screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DisplayPage(
+                      name: _nameController.text,
+                      surname: _surnameController.text,
+                      day: _dayController.text,
+                      month: _monthController.text,
+                      year: _yearController.text,
+                    ),
+                  ),
+                );
+              },
+              child: Text("Submit"),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class DisplayPage extends StatelessWidget {
+  final String name;
+  final String surname;
+  final String day;
+  final String month;
+  final String year;
+
+  const DisplayPage({
+    required this.name,
+    required this.surname,
+    required this.day,
+    required this.month,
+    required this.year,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
       body: Center(
-  
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Container(
+          width: 200,
+          height: 150,
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Ism: $name',
+                    style: TextStyle(fontSize: 18, color: Colors.white)),
+                Text('Familiya: $surname',
+                    style: TextStyle(fontSize: 18, color: Colors.white)),
+                Text('Kun: $day',
+                    style: TextStyle(fontSize: 18, color: Colors.white)),
+                Text('Oy: $month',
+                    style: TextStyle(fontSize: 18, color: Colors.white)),
+                Text('Yil: $year',
+                    style: TextStyle(fontSize: 18, color: Colors.white)),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
